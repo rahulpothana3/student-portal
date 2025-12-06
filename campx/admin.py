9a8de4f3b1375482ac91ceab04ec3977d876862e
rahul <rahulpothana3@gmail.com>
2025-12-06 15:37:11 +0530

    added again

--- campx/admin.py
+++ campx/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import teacherinfo
+
+admin.site.register(teacherinfo)
 
 # Register your models here.
--- campx/models.py
+++ campx/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class teacherinfo(models.Model):
+    fullname: models.TextField(max_length=100)
+    email:models.EmailField()
+    studentid:models.TextField()
+    department:models.TextField()
+    password:models.TextField()
+    confirmpassword:models.TextField()
+    def ___str___(self):
+        return self.fullname
+
+
+
